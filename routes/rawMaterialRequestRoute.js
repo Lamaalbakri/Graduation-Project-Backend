9540912584aaddbb8028f8d1a17fa310c8b2410e
Lamya <lamya.lafi.sj@gmail.com>
2024-09-26 02:01:32 +0300

    create getRawMaterialRequestById ,getRawMaterialRequests, and getRawMaterialRequestByMSlug

--- routes/rawMaterialRequestRoute.js
+++ routes/rawMaterialRequestRoute.js
@@ -1,11 +1,14 @@
 const express = require('express');//import the express library
 
-const { getRawMaterialRequest, createRawMaterialRequest } = require('../services/rowMaterialRequestService');
+const { getRawMaterialRequests, createRawMaterialRequest, getRawMaterialRequestById, getRawMaterialRequestByMSlug } = require('../Services/rowMaterialRequestService');
 
 
 const router = express.Router();
 
 //Routes
-router.route('/').get(getRawMaterialRequest).post(createRawMaterialRequest);
+router.route('/').get(getRawMaterialRequests).post(createRawMaterialRequest);
+router.route('/:id').get(getRawMaterialRequestById); // Define a special path for ObjectId
+router.route('/manufacturer/slug/:slug').get(getRawMaterialRequestByMSlug); // Path to search for manufacturer slug
+
 
 module.exports = router;
--- services/rowMaterialRequestService.js
+++ services/rowMaterialRequestService.js
@@ -1,15 +1,56 @@
 const slugify = require('slugify');
 const asyncHandler = require('express-async-handler')
 const RawMaterialRequestModel = require('../models/RawMaterialRequestModel');
+const mongoose = require('mongoose');
 
-exports.getRawMaterialRequest = (req, res) => {
-    // const name = req.body.name;
-    // console.log(req.body);
-    res.send();
-};
+// @desc Get list of Raw Material Request 
+// @route GET /api/v1/rawMaterialRequest
+// @access Public
+exports.getRawMaterialRequests = asyncHandler(async (req, res) => {
+    const page = req.query.page * 1 || 1;//To divide data into pages
+    const limit = req.query.limit * 1 || 5;//Number of data to appear on each page
+    const skip = (page - 1) * limit;//If I was on page  (2), I would skip the number of previous pages minus the page I am on multiplied by the number of data in it.
+    const RawMaterialRequests = await RawMaterialRequestModel.find({}).skip(skip).limit(limit);
+    res.status(200).json({ results: RawMaterialRequests.length, page, data: RawMaterialRequests });
+});
 
+// @desc Get Specific Raw Material Request by id
+// @route GET /api/v1/rawMaterialRequest/ :id
+// @access Public
+exports.getRawMaterialRequestById = asyncHandler(async (req, res) => {
+    const { id } = req.params;//take id from / :id
 
-// create Raw Material Request  
+    //Check if the id is valid as an ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ msg: 'Invalid ID format' });
+    }
+    const request = await RawMaterialRequestModel.findById(id);
+
+    //check if the request is null or undefined
+    if (!request) {
+        return res.status(404).json({ msg: `There is no Request for this id: ${id}` });
+    }
+    res.status(200).json({ data: request });
+});
+
+// @desc Get Specific Raw Material Request by manufacturer name "slug"
+// @route GET /api/v1/rawMaterialRequest/manufacturer/slug/:slug
+// @access Public
+exports.getRawMaterialRequestByMSlug = asyncHandler(async (req, res) => {
+    const { slug } = req.params;//take  slug from / :slug
+
+    const request = await RawMaterialRequestModel.findOne({ slug });
+
+    //check if the request is null or undefined
+    if (!request) {
+        return res.status(404).json({ msg: `There is no Request for this manufacturer slug: ${slug}` });
+    }
+    res.status(200).json({ data: request });
+});
+
+// @desc Create Raw Material Request 
+// @route POST /api/v1/rawMaterialRequest
+// @access Public
 exports.createRawMaterialRequest = asyncHandler(async (req, res) => {
     const manufacturerName = req.body.manufacturerName;
     const supplyingItems = req.body.supplyingItems;
